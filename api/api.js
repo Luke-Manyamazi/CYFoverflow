af0d122296a8c2a3b64983361267b1d84ec1fbdd
sheetalkharab <sheetalrana89@gmail.com>
2025-11-21 13:30:12 +0000

    end point for questionapi

--- api/api.js
+++ api/api.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import messageRouter from "./messages/messageRouter.js";
+import questionRouter from "./questions/questionRouter.js";
 
 const api = Router();
 
 api.use("/message", messageRouter);
+api.use("/questions", questionRouter);
 
 export default api;
