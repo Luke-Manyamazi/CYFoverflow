ab7955244b08c7cd7af708d4f1a85a073230678a
Alaa <aliqassabuk@gmail.com>
2025-11-22 14:34:07 +0000

    fix api URL

--- api/auth/authRepository.js
+++ api/auth/authRepository.js
@@ -1,14 +1,14 @@
 import db from "../db.js";
 
 export async function findUserByEmail(email) {
-	const { result } = await db.query("SELECT * FROM users WHERE email = $1", [
+	const result = await db.query("SELECT * FROM users WHERE email = $1", [
 		email,
 	]);
 	return result.rows[0];
 }
 
 export async function createUser(name, email, hashedPassword) {
-	const { result } = await db.query(
+	const result = await db.query(
 		"INSERT INTO users (name, email, hashed_password) VALUES ($1, $2, $3) RETURNING id, name, email",
 		[name, email, hashedPassword],
 	);
@@ -16,7 +16,7 @@ export async function createUser(name, email, hashedPassword) {
 }
 
 export async function findUserById(id) {
-	const { result } = await db.query(
+	const result = await db.query(
 		"SELECT id, name, email FROM users WHERE id = $1",
 		[id],
 	);
