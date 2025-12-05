1c40312f664fde1fdee9287b683417847de4035f
Luke Manyamazi <lukemanyamazi1@gmail.com>
2025-12-05 15:24:54 +0200

    fixed vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -9,8 +9,10 @@ export default defineConfig({
 	},
 	server: {
 		proxy: {
-			"/api": "http://localhost:3000",
-
+			"/api": {
+				target: "http://localhost:8080",
+				changeOrigin: true,
+			},
 		},
 	},
 });
