5adfb637551b52d67925f4a77e479f3e288e5adc
eyuell21 <abrahameyuel4@gmail.com>
2025-12-12 14:51:27 +0000

    Minor fixes

--- api/vitest.config.js
+++ api/vitest.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		globals: true, // allows describe, it, expect without import
-		environment: "node", // Node environment for Express tests
+		globals: true,
+		environment: "node",
 		include: ["**/__tests__/**/*.test.js"],
 	},
 });
